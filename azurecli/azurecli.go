8d8690ef20e8eca2b93b9ea6bc50c43e5ea9de4e
Stefan Kürzeder <info@stivik.me>
2023-08-09 11:46:52 +0200

    feat: Added additional warning if tenants require MFA

--- azurecli/azurecli.go
+++ azurecli/azurecli.go
@@ -1,9 +1,12 @@
 package azurecli
 
 import (
+	"bytes"
 	"errors"
 	"os"
+	"strings"
 
+	"github.com/whiteducksoftware/azctx/log"
 	"github.com/whiteducksoftware/azctx/utils"
 
 	"github.com/spf13/afero"
@@ -54,11 +57,37 @@ func (cli *CLI) Refresh() error {
 
 // Login executes the az login command
 func (cli CLI) Login(extraArgs []string) error {
+	// Create a buffer to store the stdErr output
+	var stdErrBuffer bytes.Buffer
+
+	// Execute the az login command
 	args := []string{"login"}
 	args = append(args, extraArgs...)
-	err := utils.ExecuteCommandBare(AZ_COMMAND, os.Stdout, os.Stderr, args...)
+	err := utils.ExecuteCommandBare(AZ_COMMAND, os.Stdout, &stdErrBuffer, args...)
 	if err != nil {
+		log.Error("Failed to execute 'az login': %s", err)
 		return err
+	}
+
+	// Convert the stdErr buffer to a string
+	stdErrString := stdErrBuffer.String()
+
+	// Only print lines starting with "WARNING:" to the user
+	lines := strings.Split(stdErrString, "\n")
+	for _, line := range lines {
+		// Check if the line starts with "WARNING:" but ignore the "A web browser has been opened at" line
+		if strings.HasPrefix(line, "WARNING:") && !strings.Contains(line, "A web browser has been opened at") {
+			// Remove the "WARNING: " prefix and print the line
+			log.Warn(strings.TrimPrefix(line, "WARNING: "))
+		}
+	}
+
+	// Check if the output contains "mfa" or "multi-factor authentication"
+	stdErrLower := strings.ToLower(stdErrString)
+	if strings.Contains(stdErrLower, "mfa") || strings.Contains(stdErrLower, "multi-factor authentication") {
+		log.Warn(strings.Repeat("-", 80))
+		log.Warn("Multi-factor authentication is required. Please run 'azctx login -- --tenant TENANT_ID' to login to a specific tenant using MFA. See above output for the tenant ids.")
+		log.Warn(strings.Repeat("-", 80))
 	}
 
 	return nil
